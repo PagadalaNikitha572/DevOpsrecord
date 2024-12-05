c9dadb7bcb659e289074cdbe34849941681b56ae
PagadalaNikitha572 <147132859+PagadalaNikitha572@users.noreply.github.com>
2024-12-05 10:32:10 +0530

    Update two.java

--- two.java
+++ two.java
@@ -1,5 +1,16 @@
 class two{
   public static void main(String args[]){
-    System.out.print("hoiii");
+    System.out.println("Fibonacci Series:");
+    int n1=0;
+    int n2=1;
+    int n3;
+    System.out.println(n1);
+    System.out.println(n2);
+    for(int i=0;i<10;i++){
+        n3=n1+n2;
+        System.out.println(n3);
+        n1=n2;
+        n2=n3;
+        
   }
 }
